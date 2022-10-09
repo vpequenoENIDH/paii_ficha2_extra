1968bd269b28399343b0bf6f4adf994b94298f64
Valeria Pequeno <69013969+vpequeno@users.noreply.github.com>
2022-10-09 14:24:00 +0100

    Initial commit

---
+++ src/Ficha2.java
@@ -0,0 +1,8 @@
+/**
+ * Autor:
+ * Data:
+ * Finalidade do programa: uso de vetores e matrizes em java, criação de testes automáticos com JUnit
+ */
+public class Ficha2 {
+    //Crie os métodos pedido no exercício 1 aqui
+}
---
+++ teste/TestFicha2.java
@@ -0,0 +1,2 @@
+public class TestFicha2 {
+}
